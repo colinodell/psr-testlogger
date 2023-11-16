dbb57974ba35750a99974af02ac878cfdbd3da52
JeanBaptisteRenard <140425495+JeanBaptisteRenard@users.noreply.github.com>
2023-11-17 00:31:42 +0100

    fix(TestLogger): better way to handle Exception type records

--- src/TestLogger.php
+++ src/TestLogger.php
@@ -78,7 +78,7 @@ final class TestLogger extends AbstractLogger
 
         $record = [
             'level' => $level,
-            'message' => (string) $message,
+            'message' => $message,
             'context' => $context,
         ];
 
@@ -101,7 +101,7 @@ final class TestLogger extends AbstractLogger
         }
 
         return $this->hasRecordThatPasses(static function (array $rec) use ($record) {
-            if ($rec['message'] !== $record['message']) {
+            if ((string) $rec['message'] !== (string) $record['message']) {
                 return false;
             }
 
@@ -112,14 +112,14 @@ final class TestLogger extends AbstractLogger
     public function hasRecordThatContains(string $message, string|int $level): bool
     {
         return $this->hasRecordThatPasses(static function (array $rec) use ($message) {
-            return \str_contains($rec['message'], $message);
+            return \str_contains((string) $rec['message'], $message);
         }, $level);
     }
 
     public function hasRecordThatMatches(string $regex, string|int $level): bool
     {
         return $this->hasRecordThatPasses(static function ($rec) use ($regex) {
-            return \preg_match($regex, $rec['message']) > 0;
+            return \preg_match($regex, (string) $rec['message']) > 0;
         }, $level);
     }
 
